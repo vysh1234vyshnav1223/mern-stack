5315cffe9d9de4881a598675427bc3eba3ace5e5
Vyshnav <vyshnavnair378@gmail.com>
2023-04-12 07:47:45 +0530

    Goals controller setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
 
+//initialize express
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+//listen
+app.listen(port, ()=>{
+    console.log("Server is running ")
+})
